7d62a434ad65988d620c9b895d9fc26af297ef81
Jeconiah Bautista <jeconiah.bautista@student.ateneo.edu>
2026-02-09 16:21:00 +0800

    Added context.py to store necessary contexts and templates for recipe list and ingredients.

---
+++ ledger/context.py
@@ -0,0 +1,132 @@
+def recipe_list_context():
+    ctx = {
+            "recipes": [
+                {
+                    "name": "Recipe 1",
+                    "ingredients": [
+                        {
+                            "name": "tomato",
+                            "quantity": "3pcs"
+                        },
+                        {
+                            "name": "onion",
+                            "quantity": "1pc"
+                        },
+                        {
+                            "name": "pork",
+                            "quantity": "1kg"
+                        },
+                        {
+                            "name": "water",
+                            "quantity": "1L"
+                        },
+                        {
+                            "name": "sinigang mix",
+                            "quantity": "1 packet"
+                        }
+                    ],
+                    "link": "/recipe/1"
+                },
+                {
+                    "name": "Recipe 2",
+                    "ingredients": [
+                        {
+                            "name": "garlic",
+                            "quantity": "1 head"
+                        },
+                        {
+                            "name": "onion",
+                            "quantity": "1pc"
+                        },
+                        {
+                            "name": "vinegar",
+                            "quantity": "1/2cup"
+                        },
+                        {
+                            "name": "water",
+                            "quantity": "1 cup"
+                        },
+                        {
+                            "name": "salt",
+                            "quantity": "1 tablespoon"
+                        },
+                        {
+                            "name": "whole black peppers",
+                            "quantity": "1 tablespoon"
+                        },
+                        {
+                            "name": "pork",
+                            "quantity": "1 kilo"
+                        }
+                    ],
+                    "link": "/recipe/2"
+                }
+            ]
+        }
+    return ctx
+
+def recipe_1_context():
+    ctx = {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        }
+    return ctx
+
+def recipe_2_context():
+    ctx = {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    return ctx
--- ledger/views.py
+++ ledger/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .context import recipe_list_context, recipe_1_context, recipe_2_context
 
 def recipe_list(request):
-    return HttpResponse("Recipe Book")
+    return render(request, "recipe-list.html", recipe_list_context())
 
 def recipe_1(request):
-    return HttpResponse("Recipe 1")
+    return render(request, "recipe-1.html", recipe_1_context())
 
 def recipe_2(request):
-    return HttpResponse("Recipe 2")
+    return render(request, "recipe-2.html", recipe_2_context())
 
-# Create your views here.
