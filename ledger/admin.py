057337270e8855787f2975d4bcd7c6d9cecfaf38
Jeconiah Bautista <jeconiah.bautista@student.ateneo.edu>
2026-02-19 19:01:23 +0800

    Created an admin panel for Recipe with an Inline Admin for RecipeIngredient.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Recipe, RecipeIngredient
 # Register your models here.
+
+class RecipeInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name',)
+    inlines = [RecipeInline,]
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('name',),
+            ]
+        })
+    ]
+
+admin.site.register(RecipeIngredient)
+admin.site.register(Recipe, RecipeAdmin)
--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import recipe_list, recipe_detail
 
 urlpatterns = [
-    path('recipes/list/', recipe_list, name='recipe_list'),
+    path('recipes/list/', recipe_list, name='recipe-list'),
     path('recipe/<int:pk>/', recipe_detail, name='recipe-detail'),
 ]
 
