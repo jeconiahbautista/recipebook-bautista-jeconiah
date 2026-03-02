fa4b2666df06e4b04945e128542baf90b4fa49b2
Jeconiah Bautista <jeconiah.bautista@student.ateneo.edu>
2026-03-02 17:13:32 +0800

    Created a custom login flow. Added header sections with login/logout buttons and page name display. Fixed login/lgout url redirections.

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -121,5 +121,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_REDIRECT_URL = 'recipes/list'
-LOGOUT_REDIRECT_URL = 'recipes/list'
+LOGIN_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
