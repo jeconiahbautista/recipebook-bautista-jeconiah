becf91d839278568301ae0d5260b5fdf98072e14
Jeconiah Bautista <jeconiah.bautista@student.ateneo.edu>
2026-03-02 21:10:00 +0800

    Finalization of code format and minor revisions in profile models

--- ledger/models.py
+++ ledger/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from useraccounts.models import Profile 
+from useraccounts.models import Profile
 
 
 class Ingredient(models.Model):
@@ -8,9 +8,9 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('ledger:ingredient-detail', args=[self.pk])
+        return reverse("ledger:ingredient-detail", args=[self.pk])
 
 
 class Recipe(models.Model):
@@ -18,7 +18,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
-        related_name='recipes',
+        related_name="recipes",
     )
 
     created_on = models.DateTimeField(auto_now_add=True)
@@ -26,9 +26,9 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('ledger:recipe-detail', args=[self.pk])
+        return reverse("ledger:recipe-detail", args=[self.pk])
 
 
 class RecipeIngredient(models.Model):
@@ -37,11 +37,11 @@ class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
-        related_name='recipe',
+        related_name="recipe",
     )
 
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='ingredients',
+        related_name="ingredients",
     )
--- ledger/views.py
+++ ledger/views.py
@@ -2,22 +2,19 @@ from django.shortcuts import render
 from .models import Recipe
 from django.contrib.auth.decorators import login_required
 
+
 def recipe_list(request):
     recipes = Recipe.objects.all()
 
-    ctx = {
-        "recipes": recipes
-    }
+    ctx = {"recipes": recipes}
 
     return render(request, "recipe-list.html", ctx)
+
 
 @login_required
 def recipe_detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
 
-    ctx = {
-        "recipe": recipe
-    }
+    ctx = {"recipe": recipe}
 
     return render(request, "recipe.html", ctx)
-
