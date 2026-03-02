b2adf92568c7f319ea6127d3b63f4bd615be9f32
Jeconiah Bautista <jeconiah.bautista@student.ateneo.edu>
2026-03-02 11:27:17 +0800

    Created a useraccounts app with a profile model

---
+++ useraccounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(MinLengthValidator(255))
+# Create your models here.
