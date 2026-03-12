cfd014e2e5e4968e8c837de26fbd59d5dd295c7a
Jeconiah Bautista <jeconiah.bautista@student.ateneo.edu>
2026-03-12 08:54:12 +0800

    Fixed Profile Admin View

--- useraccounts/models.py
+++ useraccounts/models.py
@@ -8,5 +8,6 @@ class Profile(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField(validators=[MinLengthValidator(256)])
 
-
+    def __str__(self):
+        return self.name
 # Create your models here.
